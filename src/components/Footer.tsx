5da938f0ebf2de6c6ba871de78d0b28688d25cba
SeattleTranslation <wentranslate@gmail.com>
2025-07-08 00:23:38 -0700

    update footer, removed red note info

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,8 +9,6 @@ const Footer: React.FC = () => {
                         <p><strong>联系方式</strong></p>
                         <p>微信：winnieaaaaa111</p>
                         <p>邮箱：wentranslate@gmail.com</p>
-                        <p>无法回复小红书私信：3814236872</p>
-
                     </div>
                 </div>
                 <p className="copyright">© {new Date().getFullYear()} 西雅图翻译💫. 保留所有权利.</p>
